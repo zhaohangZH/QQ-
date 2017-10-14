a294feb9c1b295abab0febf54a6b2a4ae043099b
zhaohangZH <1291245447@qq.com>
2017-10-14 11:06:17 +0800

    initial

--- js/index.js
+++ js/index.js
@@ -6,24 +6,22 @@ var renderZhao=(function () {
         $Release=$('#release'),//内容中间发布部分
         $Inputbox=$('#inputbox'),//发表输入框
         $Emoticon=$('#emoticon'),//表情包按钮
-        $Emobox=$('.emoticon'),//表情包盒子
+        $Emobox=$('.emoticon'),//隐藏表情包盒子
         $flip=$Emobox.find('#flip'),//表情包上下翻页按钮
         $flipa=$flip.children('a'),//表情包上下翻页a
         $flipp=$flip.children('span'),//表情包上下翻页数
-        $Bq=$Emobox.find('.bq'),
+        $Bq=$Emobox.find('.bq'),//表情包ul
         $Closeemoticon=$('#closeemoticon'),//关闭表情包
-        $Published=$('#published'),//发表按钮
-        $Frame=$Release.find('.frame'),
-        $Review=$Release.find('.review');
+        $Published=$('#published');//发表按钮
 
     //导航栏li下拉列表
     function navList(){
         $Navlist.each(function () {
             $(this).mouseenter(function () {
-                $(this).children('.waibu').css('display')==='none'?$(this).children('.waibu').slideDown(150)&&$(this).css('background','#fff')&&$(this).children('a').css('color','#000'):null;
+                $(this).children('.waibu').css('display')==='none'?$(this).children('.waibu').show(200)&&$(this).css('background','#fff')&&$(this).children('a').css('color','#000'):null;
             });
             $(this).mouseleave(function () {
-                $(this).children('.waibu').css('display')==='block'?$(this).children('.waibu').slideUp(150)&&$(this).css('background','')&&$(this).children('a').css('color',''):null;
+                $(this).children('.waibu').css('display')==='block'?$(this).children('.waibu').hide(200)&&$(this).css('background','')&&$(this).children('a').css('color',''):null;
 
             });
         });
@@ -89,8 +87,8 @@ var renderZhao=(function () {
                 }
                 $flipp.html()==1?$flipa.eq(0).addClass('no'):$flipa.eq(0).removeClass('no');
                 $flipp.html()==5?$flipa.eq(1).addClass('no'):$flipa.eq(1).removeClass('no');
-                $Bq.eq($flipp.html()-1).siblings().removeClass('bqk');
-                $Bq.eq($flipp.html()-1).addClass('bqk');
+                $Bq.eq($flipp.html()-1).siblings().removeClass('bq-block');
+                $Bq.eq($flipp.html()-1).addClass('bq-block');
             });
         })
     }
@@ -101,9 +99,9 @@ var renderZhao=(function () {
                 htm=$Inputbox.html(),
                 htm=htm.replace(/&nbsp;+/g,' '),
                 reg=/[\u4E00-\u9FFF]|\d+|[a-zA-Z]/g,
-            str='<div class="frame"><div class="framehead clear"><div class="lt clear"></div><span>学霸爹</span><span>00:00</span><div class="rt"></div></div><div class="txt">'+htm+'</div><!--发表图片区域--><div class="picture"></div><!--发表点赞、评论、转发--><div class="forward" ><p>浏览<span>555</span>浏览次</p><a href=""></a><a href=""></a><a href=""></a></div><!--发表评论--><div class="comment"><div class="review" contenteditable="true"></div><i></i></div><!--发表按钮--><div class="issued"><p></p><p></p><span>发表</span></div></div></div>';
+            str='';
+            str+='<div class="frame"><div class="framehead clear"><div class="lt clear"></div><span>学霸爹</span><span>00:00</span><div class="rt"></div></div><div class="txt">'+htm+'</div><!--发表图片区域--><div class="picture"></div><!--发表点赞、评论、转发--><div class="forward" ><p>浏览<span>555</span>浏览次</p><a href=""></a><a href=""></a><a href=""></a></div><!--发表评论--><div class="comment"><div class="review" contenteditable="true"></div><i></i></div><!--发表按钮--><div class="issued"><p></p><p></p><span>发表</span></div></div></div>';
             reg.test(txt)? $Release.append(str)&&$Inputbox.html(''):$Inputbox.blur()&&$Inputbox.html('');
-            //console.log($Release.html());
         });
         $Release.on('click','.review',function (e) {
             e.stopPropagation();
